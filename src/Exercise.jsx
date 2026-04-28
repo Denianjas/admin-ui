3a8107b73bb8c63b01a07375272e7444ad4a59f7
denianjas <Demarsia159@gmail.com>
2026-04-28 13:18:37 +0700

    TUGAS UTS V0.1

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import PostCard from './PostCard'; 
-import { postsData } from "./postsData"; // Pastikan ini sesuai dengan nama file/variabel Anda
+import PostCard from './PostCard';
+import { postsData } from './postsData';
 
 function Exercise() {
   return (
     <div className="min-h-screen bg-gray-50 p-6">
       
-      {/* Judul Utama dengan warna Special Red2 */}
+      
       <h1 className="text-2xl font-bold text-center mb-8 text-[#b83016]">
         Post Cards
       </h1>
       
-      {/* Grid diset ke 6 kolom untuk layar extra large (xl:grid-cols-6) */}
+      
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
         {postsData.map((post) => (
           <PostCard 
